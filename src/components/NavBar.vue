7f40858ae68d5168fafd397367becdd2ed2527b5
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-01 18:33:58 +0200

    Create NavBar.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import { NAV } from "@/lib/constants";
+</script>
+
+<template>
+  <header class="max-w-4xl mx-auto my-8">
+    <nav>
+      <ul class="flex items-center gap-x-3 text-white text-xl">
+        <li v-for="item in NAV" :key="item.id" class="glow font-semibold">
+          <RouterLink :to="item.href">
+            {{ item.name }}
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>

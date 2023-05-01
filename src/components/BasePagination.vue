96df3d01f404ebbf143e07d6102e0cd3bdb369ce
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-01 18:34:01 +0200

    Create BasePagination.vue

---
+++ src/components/BasePagination.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+type Props = {
+  currentPage: number;
+  pages?: number;
+};
+
+type Emits = {
+  (e: "goToPreviousPage"): void;
+  (e: "goToNextPage"): void;
+};
+
+defineProps<Props>();
+const emit = defineEmits<Emits>();
+</script>
+
+<template>
+  <div class="flex justify-start flex-1 gap-x-2">
+    <button
+      class="cursor-pointer rounded-md border border-pink-400 bg-pink-800/90 py-1.5 px-3 text-gray-200 hover:bg-pink-800/70 disabled:cursor-not-allowed disabled:bg-pink-900"
+      :disabled="currentPage === 1"
+      @click="emit('goToPreviousPage')"
+    >
+      Previous
+    </button>
+    <button
+      class="cursor-pointer rounded-md border border-pink-400 bg-pink-800/90 py-1.5 px-3 text-gray-200 hover:bg-pink-800/70 disabled:cursor-not-allowed disabled:bg-pink-900"
+      :disabled="currentPage === pages"
+      @click="emit('goToNextPage')"
+    >
+      Next
+    </button>
+  </div>
+</template>

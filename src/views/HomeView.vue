bf80a826b00d1454a6d368d552bd7d82f978b20a
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-01 18:34:26 +0200

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+onMounted(() => {
+  router.push({ name: "characters", query: { page: 1 } });
+});
+</script>
+
+<template>
+  <div></div>
+</template>
